8a6910cec4b27a0ab49a65b115bff9bfab18ef45
nikikiarie <110396228+nikikiarie@users.noreply.github.com>
2025-05-07 00:36:16 +0300

    Update mpesa.js

--- server/routes/mpesa.js
+++ server/routes/mpesa.js
@@ -27,7 +27,7 @@ router.post('/callback', async (req, res) => {
     const callbackData = req.body;
 
     
-   // 1. First show me EVERYTHING as-is
+  /* // 1. First show me EVERYTHING as-is
     console.log("COMPLETE CALLBACK DATA:\n", JSON.stringify(callbackData, null, 2));
     
     // 2. Then show just the metadata structure
@@ -37,8 +37,8 @@ router.post('/callback', async (req, res) => {
     } else {
         console.log("\nNo metadata found");
     }
-
-  /*
+*/
+  
   const checkoutId = callbackData?.Body?.stkCallback?.CheckoutRequestID
   try {
     const order = await Order.findOne({ checkoutRequestId: checkoutId });
@@ -68,7 +68,7 @@ router.post('/callback', async (req, res) => {
         console.log("Updated order:", order);
   } catch (err) {
     console.error("Error finding order:", err);
-} */
+}
     
     // res.status(200).end();
 
