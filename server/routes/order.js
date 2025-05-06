57865d35a7c9823cfa642ad22474df72becb0dc5
nikikiarie <110396228+nikikiarie@users.noreply.github.com>
2025-05-07 00:22:29 +0300

    Update order.js

--- server/routes/order.js
+++ server/routes/order.js
@@ -9,6 +9,7 @@ const router = require('express').Router()
 router.post('/', async (req, res) => {
   try {
     const { userId, items, totalAmount } = req.body;
+    console.log(items)
     
     const order = await Order.create({
       userId,
